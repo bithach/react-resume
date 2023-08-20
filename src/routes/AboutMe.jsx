e90331cc3880267f54f110c1b6ab17f8c9136f30
Thach Huynh <thh190001@utdallas.edu>
2023-08-20 17:53:04 -0500

    added about me route

---
+++ src/routes/AboutMe.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Slideshow from "../components/Slideshow";
+
+function AboutMe(props) {
+  return <Slideshow></Slideshow>;
+}
+
+export default AboutMe;
