9c463d4e7799432cbf186363c7c712e636f9fb8a
Thach Huynh <thh190001@utdallas.edu>
2023-10-04 13:25:11 -0500

    moving around stuff

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,20 +6,19 @@ import Col from "react-bootstrap/Col";
 function HeroSection(props) {
   return (
     <Container>
-      <Row>
-        <Col style={{ height: 100 + "%" }}>
+      <Row style={{ padding: "10px 0 0 0" }}>
+        <Col style={{ height: 100 + "%", textAlign: "center" }}>
           <div className="heroText">
             <h1>THACH HUYNH</h1>
+            <br />
             <p>Striving for something new</p>
           </div>
         </Col>
         <Col>
-          <div className="selfIamge">
-            <img
-              src={require("../../src/styling/images/P1020066.JPG")}
-              alt="Thach Huynh"
-            />
-          </div>
+          <img
+            src={require("../../src/styling/images/IMG_3938.JPG")}
+            alt="Thach Huynh"
+          />
         </Col>
       </Row>
     </Container>
