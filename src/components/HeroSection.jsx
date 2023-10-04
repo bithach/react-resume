27e00591912bc6c52b26f7b1c5ae10fae2701141
Thach Huynh <thh190001@utdallas.edu>
2023-10-04 12:41:50 -0500

    added image and text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,29 @@
-import "../styling/HeroSection.css"
+import "../styling/HeroSection.css";
+import { Container } from "react-bootstrap/";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function HeroSection(props){
-    return(
-        <div>
+function HeroSection(props) {
+  return (
+    <Container>
+      <Row>
+        <Col style={{ height: 100 + "%" }}>
+          <div className="heroText">
             <h1>THACH HUYNH</h1>
             <p>Striving for something new</p>
-            <img src="src\styling\images\P1020066.JPG"/>
-        </div>
-    )
+          </div>
+        </Col>
+        <Col>
+          <div className="selfIamge">
+            <img
+              src={require("../../src/styling/images/P1020066.JPG")}
+              alt="Thach Huynh"
+            />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default HeroSection;
