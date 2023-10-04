3eb4bf9ca61e00477a2a5e325734720d6e14c47b
Thachikas <bithach@hotmail.com>
2023-10-04 11:59:43 -0500

    adding hero sectoin

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,15 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "../styling/Home.css";
+import "../components/AboutMeSection"
+import HeroSection from "../components/HeroSection";
 
 function Home(props) {
   return (
-    <div className="background-image">
-      <div className="intro-text">
-        <h1>Welcome to my website</h1>
-        <a href="/AboutMe">About Me</a>
-      </div>
-    </div>
+    <>
+      <HeroSection/>
+    </>
   );
 }
 
