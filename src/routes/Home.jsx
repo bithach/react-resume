a20abae70835ad5c9b0992396ce7778717d39263
Thachikas <bithach@hotmail.com>
2023-10-11 16:07:51 -0500

    addeda AboutMe Section

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import "../styling/Home.css";
 import "../components/AboutMeSection"
 import HeroSection from "../components/HeroSection";
+import AboutMeSection from "../components/AboutMeSection";
 
 function Home(props) {
   return (
     <>
       <HeroSection/>
+      <AboutMeSection/>
     </>
   );
 }
