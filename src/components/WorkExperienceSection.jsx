9ddb3caa9c6b8af415d5e8e6971182d7d8501e23
Thachikas <bithach@hotmail.com>
2023-10-11 17:53:14 -0500

    added useState for future buttons

--- src/components/WorkExperienceSection.jsx
+++ src/components/WorkExperienceSection.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "../styling/WorkExperienceSection.css"
 import { Container } from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
 function WorkExperienceSection(props){
+    const [value, setValue] = useState([1, 3]);
+
+    function onClick(){
+        return;
+    }
+
     return(
         <div className="workExeperience-body">
             <div className="workExeperience-title-desktop">
