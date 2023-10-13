8738f2f1a80cf8e5a8c65ad8ccc8a740e7d33c6d
Thachikas <bithach@hotmail.com>
2023-10-12 19:19:29 -0500

    some layout of work experience

--- src/components/WorkExperienceSection.jsx
+++ src/components/WorkExperienceSection.jsx
@@ -1,20 +1,45 @@
 import { useState } from "react";
-import "../styling/WorkExperienceSection.css"
-import { Container } from "react-bootstrap/Container";
+import { Container } from "react-bootstrap/";
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+import "../styling/WorkExperienceSection.css"
 
 function WorkExperienceSection(props){
-    const [value, setValue] = useState([1, 3]);
+    const [value, setValue] = useState(0);
 
-    function onClick(){
+    function companyButtonClick(event){
+        setValue(a => event.target.id)
+        event.preventDefault();
         return;
     }
 
     return(
         <div className="workExeperience-body">
-            <div className="workExeperience-title-desktop">
-                <h1>/* Work Experience */</h1>
+            <h1 className="workExeperience-title-desktop">/* Work Experience */</h1>
+            
+            <div className="company-buttons">
+                <Button onClick={companyButtonClick} id={1}>Company Name</Button>
+                <Button onClick={companyButtonClick} id={2}>Company Name</Button>
+                <Button>Company Name</Button>
+            </div>
+
+            <div className="company-box">
+                <Container>
+                    <Row>
+                        {/*This is for the description*/}
+                        <Col style={{textAlign:"center"}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut faucibus pulvinar. Imperdiet massa tincidunt nunc pulvinar sapien. Nunc sed velit dignissim sodales ut eu. Viverra adipiscing at in tellus integer feugiat scelerisque varius. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Lectus arcu bibendum at varius vel pharetra. Pharetra convallis posuere morbi leo urna molestie at elementum. Eu mi bibendum neque egestas congue quisque. Netus et malesuada fames ac. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Mauris vitae ultricies leo integer. Egestas pretium aenean pharetra magna ac placerat. Faucibus et molestie ac feugiat sed lectus vestibulum mattis. Pulvinar pellentesque habitant morbi tristique senectus et netus. Vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor. Odio ut sem nulla pharetra diam. Aliquam eleifend mi in nulla posuere sollicitudin. Aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros.
+                        </Col>
+                        {/*This is for the company logo*/}
+                        <Col style={{textAlign: "center"}}>
+                            Image here
+                        </Col>
+                    </Row>
+                    <Row style={{borderTop:"solid black 3px"}}>
+                        
+                    </Row>
+                </Container>
             </div>
         </div>
     )
