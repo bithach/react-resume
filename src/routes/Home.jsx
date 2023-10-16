d231e0d40c6b269010c527021a6331e6e8461314
Thach Huynh <thh190001@utdallas.edu>
2023-10-16 16:52:01 -0500

    added padding for info box

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "../styling/Home.css";
-import "../components/AboutMeSection";
-import HeroSection from "../components/HeroSection";
-import AboutMeSection from "../components/AboutMeSection";
-import WorkExperienceSection from "../components/WorkExperienceSection";
-import ProjectsSection from "../components/ProjectsSection";
+import HeroSection from "../sections/HeroSection";
+import AboutMeSection from "../sections/AboutMeSection";
+import WorkExperienceSection from "../sections/WorkExperienceSection";
+import ProjectsSection from "../sections/ProjectsSection";
 
 function Home(props) {
   return (
