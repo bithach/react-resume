b71455dc1d65a8bcdd2036c93280b9398b18ccd3
Thachikas <bithach@hotmail.com>
2023-10-18 14:31:05 -0500

    added cards and media query

--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import "../styling/ProjectsSection.css";
 import Card from "react-bootstrap/Card";
-import { Button } from "react-bootstrap";
-import { Container } from "react-bootstrap/Container";
+import { Button, CardGroup } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "../styling/ProjectsSection.css";
 
 function ProjectsSection(props) {
   const [value, setValue] = useState([1, 3]);
@@ -19,17 +19,38 @@ function ProjectsSection(props) {
         <h1>/* Projects */</h1>
       </div>
       <div>
-        <Card className="text-center mx-auto" style={{ width: "15rem" }}>
-          <Card.Img
-            src="https://cdn-icons-png.flaticon.com/512/3142/3142722.png"
-            style={{ padding: "10px 0px" }}
-          />
-          <Card.Title>Brown Bag</Card.Title>
-          <Card.Text>Loren ipsum</Card.Text>
-          <div>
-            <Button style={{ alignItems: "flex-start" }}>Github</Button>
-          </div>
-        </Card>
+        <span>
+          <Row className="m-auto">
+            <Card className="text-center mx-auto px-0 project-card-desktop">
+              <Card.Img
+                src="https://cdn-icons-png.flaticon.com/512/3142/3142722.png"
+                style={{ padding: "10px 0px" }}
+              />
+              <Card.Title>Brown Bag</Card.Title>
+              <Card.Text>Loren ipsum</Card.Text>
+              <div style={{padding:"0px 0px 5px"}}>
+                <Button style={{ alignItems: "flex-start" }}>Github</Button>
+              </div>
+              <Card.Footer>
+                <div>Python</div>
+              </Card.Footer>
+            </Card>
+            <Card className="text-center mx-auto px-0 project-card-desktop">
+              <Card.Img
+                src="https://cdn-icons-png.flaticon.com/512/3142/3142722.png"
+                style={{ padding: "10px 0px" }}
+              />
+              <Card.Title>Venmo Scraper</Card.Title>
+              <Card.Text>Loren ipsum</Card.Text>
+              <div style={{padding:"0px 0px 5px"}}>
+                <Button style={{ alignItems: "flex-start" }}>Github</Button>
+              </div>
+              <Card.Footer>
+                <div>Python</div>
+              </Card.Footer>
+            </Card>
+          </Row>
+        </span>
       </div>
     </div>
   );
