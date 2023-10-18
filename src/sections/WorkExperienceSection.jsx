0c00a595bdef9a79c7817f64bb6b61a3b10c2427
Thachikas <bithach@hotmail.com>
2023-10-18 14:31:29 -0500

    media query and change upon button click

--- src/sections/WorkExperienceSection.jsx
+++ src/sections/WorkExperienceSection.jsx
@@ -4,12 +4,17 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import "../styling/WorkExperienceSection.css";
+import { experienceData } from "../data/WorkExperienceData"
 
 function WorkExperienceSection(props) {
   const [value, setValue] = useState(0);
+  const [experienceDescription, changeDescription] = useState("");
 
   function companyButtonClick(event) {
     setValue((a) => event.target.id);
+    console.log(experienceData)
+    console.log(experienceData[value-1].description)
+    changeDescription((b)=>experienceData[value-1].description)
     event.preventDefault();
     return;
   }
@@ -20,12 +25,8 @@ function WorkExperienceSection(props) {
 
       <div className="company-buttons">
         <Button onClick={companyButtonClick} id={1}>
-          Company Name
+          USAA
         </Button>
-        <Button onClick={companyButtonClick} id={2}>
-          Company Name
-        </Button>
-        <Button>Company Name</Button>
       </div>
 
       <div className="company-box-container-desktop">
@@ -34,24 +35,7 @@ function WorkExperienceSection(props) {
             <Row>
               {/*This is for the description*/}
               <Col style={{ textAlign: "center" }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Sagittis id consectetur purus ut faucibus pulvinar. Imperdiet
-                massa tincidunt nunc pulvinar sapien. Nunc sed velit dignissim
-                sodales ut eu. Viverra adipiscing at in tellus integer feugiat
-                scelerisque varius. Laoreet id donec ultrices tincidunt arcu non
-                sodales neque sodales. Lectus arcu bibendum at varius vel
-                pharetra. Pharetra convallis posuere morbi leo urna molestie at
-                elementum. Eu mi bibendum neque egestas congue quisque. Netus et
-                malesuada fames ac. Commodo odio aenean sed adipiscing diam
-                donec adipiscing tristique risus. Mauris vitae ultricies leo
-                integer. Egestas pretium aenean pharetra magna ac placerat.
-                Faucibus et molestie ac feugiat sed lectus vestibulum mattis.
-                Pulvinar pellentesque habitant morbi tristique senectus et
-                netus. Vestibulum lorem sed risus ultricies tristique nulla
-                aliquet enim tortor. Odio ut sem nulla pharetra diam. Aliquam
-                eleifend mi in nulla posuere sollicitudin. Aenean pharetra magna
-                ac placerat vestibulum lectus mauris ultrices eros.
+                {experienceDescription}
               </Col>
               {/*This is for the company logo*/}
               <Col style={{ textAlign: "center" }}>Image here</Col>
