b22d9b825fa76e85e2a0c5e5f1c8ad0b88d7a764
Thachikas <bithach@hotmail.com>
2023-10-18 15:40:51 -0500

    added USAA logo

--- src/data/WorkExperienceData.jsx
+++ src/data/WorkExperienceData.jsx
@@ -2,7 +2,8 @@ export let experienceData =
 [{
     id:1,
     companyName:"USAA",
-    description:"Lorum ipsum"
+    description:"Lorum ipsum",
+    logo: "/src/styling/images/Usaa_logo_PNG2.png"
 
 },
 {
