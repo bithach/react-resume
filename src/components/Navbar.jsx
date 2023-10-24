2fdd742f34ce9d8eb859837ffb11a9737fa19c97
Thachikas <bithach@hotmail.com>
2023-10-24 16:45:39 -0500

    added Navbar
    recommended by professor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-import "./styling/Navbar.css";
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
-function Navbar(props) {
-  return <></>;
+function NavbarComponent(props) {
+  return (
+      <>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="m-auto">
+              <Nav.Link href="#aboutme">Home</Nav.Link>
+              <Nav.Link href="#work">Experience</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>);
 }
 
-export default Navbar;
+export default NavbarComponent;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "../styling/Home.css";
+import NavbarComponent from "../components/Navbar";
 import HeroSection from "../sections/HeroSection";
 import AboutMeSection from "../sections/AboutMeSection";
 import WorkExperienceSection from "../sections/WorkExperienceSection";
@@ -9,6 +10,7 @@ import ProjectsSection from "../sections/ProjectsSection";
 function Home(props) {
   return (
     <>
+      <NavbarComponent/>
       <HeroSection />
       <AboutMeSection />
       <WorkExperienceSection />
