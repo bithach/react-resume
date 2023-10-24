62342b6ca8cf40461264646e044669a65d0412c2
Thachikas <bithach@hotmail.com>
2023-10-24 17:46:06 -0500

    added ternary operator

--- src/sections/WorkExperienceSection.jsx
+++ src/sections/WorkExperienceSection.jsx
@@ -7,17 +7,20 @@ import "../styling/WorkExperienceSection.css";
 import { experienceData } from "../data/WorkExperienceData"
 
 function WorkExperienceSection(props) {
+  const [targetID, setTargetID] = useState(0);
   const [experienceDescription, changeDescription] = useState("");
+  const [experienceLogo, changeLogo] = useState("")
 
   function companyButtonClick(event) {
     event.preventDefault()
+    setTargetID((a)=>event.target.id)
     changeDescription((b)=>experienceData[event.target.id-1].description)
     return;
   }
 
   return (
     <div className="workExeperience-body">
-      <h1 className="workExeperience-title-desktop">/* Work Experience */</h1>
+      <h1 className="workExeperience-title-desktop"><b>/* Work Experience */</b></h1>
 
       <div className="company-buttons">
         <Button onClick={companyButtonClick} id={1}>
@@ -25,25 +28,22 @@ function WorkExperienceSection(props) {
         </Button>
       </div>
 
+      {targetID !== 0 ? <>
       <div className="company-box-container-desktop">
         <div className="company-box">
           <Container fluid>
             <Row>
-              {/*This is for the description*/}
               <Col style={{ textAlign: "center" }}>
                 {experienceDescription}
               </Col>
-              {/*This is for the company logo*/}
-              <Col style={{ textAlign: "center" }}>Image here</Col>
-            </Row>
-            <Row
-              style={{ justifyContent: "center", borderTop: "solid black 3px" }}
-            >
-              This is stuff
+              <Col>
+                Image Here
+              </Col>
             </Row>
           </Container>
         </div>
-      </div>
+      </div> 
+      </>: <p style={{textAlign:"center"}}>Click on one of the buttons above!</p>}
     </div>
   );
 }
