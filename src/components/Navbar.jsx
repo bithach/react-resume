2ee5819e66dae0299985dd886084cb1a6db439f3
Thachikas <bithach@hotmail.com>
2023-10-28 22:09:34 -0500

    Updated navbar
    Added sticky top and a berakpoint for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavbarComponent(props) {
   return (
       <>
-        <Navbar bg="dark" data-bs-theme="dark">
-          <Container>
+        <Navbar sticky="top" expand="lg" bg="dark" data-bs-theme="dark">
+          <Container fluid>
             <Navbar.Brand href="#home">Thach Huynh</Navbar.Brand>
-            <Nav className="m-auto">
-              <Nav.Link href="#aboutme">About Me</Nav.Link>
-              <Nav.Link href="#work">Experience</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
-            </Nav>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="m-auto">
+                <Nav.Link href="#aboutme">About Me</Nav.Link>
+                <Nav.Link href="#workexperience">Experience</Nav.Link>
+                <Nav.Link href="#projects">Projects</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
       </>);
