6aec708c2a17ce46b26d40e8765cfb8a721ddafc
Thach Huynh <thh190001@utdallas.edu>
2023-10-31 18:26:00 -0500

    removed bootstrap

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styling/Slideshow.css";
 
 function Slideshow() {
   return (
     <div className="slides">
-      <div className="slide"/>
-      <div className="slide"/>
-      <div className="slide"/>
+      <div className="slide" />
+      <div className="slide" />
+      <div className="slide" />
     </div>
   );
 }
