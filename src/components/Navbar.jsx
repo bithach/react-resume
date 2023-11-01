dde108a47c67073bde82192293bcc265f620b9ed
Thach Huynh <thh190001@utdallas.edu>
2023-10-31 23:18:25 -0500

    Revert "Revert "removed bootstrap""
    This reverts commit 7c6693ccdb462ad9cfa18a5f3f551aadd96bbab8.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,7 @@
 import React from "react";
-import Nav from "react-bootstrap/Nav"
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavbarComponent(props) {
-  return (
-      <>
-        <Navbar sticky="top" expand="lg" bg="dark" data-bs-theme="dark">
-          <Container fluid>
-            <Navbar.Brand href="#home">Thach Huynh</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="m-auto">
-                <Nav.Link href="#aboutme">About Me</Nav.Link>
-                <Nav.Link href="#workexperience">Experience</Nav.Link>
-                <Nav.Link href="#projects">Projects</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </>);
+  return <></>;
 }
 
 export default NavbarComponent;
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,33 +1,7 @@
 import "../styling/HeroSection.css";
-import { Container } from "react-bootstrap/";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 
 function HeroSection(props) {
-  return (
-    <Container>
-      <Row className="align-items-center heroRow">
-        <Col style={{ height: 100 + "%", textAlign: "center" }}>
-          <div className="heroText">
-            <h1>THACH HUYNH</h1>
-            <br />
-            <p>Striving for something new</p>
-            <Button size="lg" variant="outline-dark">
-              Contact Me
-            </Button>
-          </div>
-        </Col>
-        <Col>
-          <img
-            src={require("../../src/styling/images/IMG_3938.JPG")}
-            alt="Thach Huynh"
-            className="img-desktop"
-          />
-        </Col>
-      </Row>
-    </Container>
-  );
+  return;
 }
 
 export default HeroSection;
