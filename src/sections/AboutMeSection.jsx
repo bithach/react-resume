35c7e8152c41a7ec1c15e8fe75abd06695c843d5
Thach Huynh <thh190001@utdallas.edu>
2023-11-05 01:41:45 -0600

    updated about me

--- src/sections/AboutMeSection.jsx
+++ src/sections/AboutMeSection.jsx
@@ -1,16 +1,18 @@
-import "../styling/AboutMeSection.css"
+import "../styling/AboutMeSection.css";
 
-function AboutMeSection(props){
-    return(
-        <div className="aboutMe-body" id="aboutme-section">
-            <div className="aboutMe-title-desktop">
-                <h1><b>/* About Me */</b></h1>
-            </div>
-            <div className="aboutMe-description-desktop">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-        </div>
-    )
+function AboutMeSection(props) {
+  return (
+    <div className="aboutMe-body" id="aboutme-section">
+      <h1 className="aboutMe-title-desktop">/* About Me *\</h1>
+      <p className="aboutMe-description-desktop">
+        I am currently a senior at the University of Texas at Dallas. I will be
+        graduating Fall 2023 with a Bachelors of Science in Software
+        Engineering. I primarly used Java and Python throughout my college
+        career. I am currently learning Javascript/Typescript in a web
+        development environemnt using a React framework.
+      </p>
+    </div>
+  );
 }
 
 export default AboutMeSection;
