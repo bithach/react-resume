af31540ca49615661114af493866b9441a42ee50
Thach Huynh <thh190001@utdallas.edu>
2023-11-05 23:31:31 -0600

    removed ids

--- src/data/WorkExperienceData.jsx
+++ src/data/WorkExperienceData.jsx
@@ -1,12 +1,8 @@
 export let experienceData = [
   {
-    id: 1,
     companyName: "USAA",
     description:
       "During my internship here, I was able to get hands on experience with React and primarily working in the Risk-Compliance department. With this experience, I became more interested in the world of web development. ",
     logo: require("../../src/styling/images/Usaa_logo_PNG2.png"),
-  },
-  {
-    id: 2,
   },
 ];
