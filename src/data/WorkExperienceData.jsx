950228312509670abd13fbe95999dbe82106554f
Thachikas <bithach@hotmail.com>
2023-11-07 15:03:38 -0600

    added more to desc

--- src/data/WorkExperienceData.jsx
+++ src/data/WorkExperienceData.jsx
@@ -2,7 +2,7 @@ export let experienceData = [
   {
     companyName: "USAA",
     description:
-      "During my internship here, I was able to get hands on experience with React and primarily working in the Risk-Compliance department. With this experience, I became more interested in the world of web development. ",
+      "During my internship here, I was able to get hands on experience with React and primarily working in the Risk-Compliance department. With this experience, I was able to learn more about web development along with using different libraries concerning visualization of data and became more interested in the world of web development. ",
     logo: require("../../src/styling/images/Usaa_logo_PNG2.png"),
   },
 ];
