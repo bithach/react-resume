c8b5c94a18b43b5d09221512f0c8d5d9883e150c
Thach Huynh <thh190001@utdallas.edu>
2023-11-13 01:10:03 -0600

    format changes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,17 +6,24 @@ export function ProjectCard(props) {
       <div
         className="project-card-container text-center w-64 max-h-[450px] max-w-64 border-2 border-black rounded-2xl bg-white 
                       transition ease-out delay-400 
-                      hover:translate-y-[-10px] hover:shadow-md hover:shadow-black"
+                      hover:translate-y-[-10px] 
+                      hover:shadow-md 
+                      hover:shadow-black"
       >
         <h3 className="project-card-name text-center font-bold py-[5px] border-b-2 border-slate-400">
           {props.projectName}
         </h3>
         <div className="flex justify-center items-center">
-          <img
-            className="project-logo w-[192px] h-[192px] p-[5px]"
-            src={props.projectLogo}
-            alt={props.projectName + " Logo"}
-          />
+          {console.log(props.projectLogo)}
+          {props.projectLogo !== "" ? (
+            <img
+              className="project-logo w-[192px] h-[192px] p-[5px]"
+              src={props.projectLogo}
+              alt=""
+            />
+          ) : (
+            ""
+          )}
         </div>
         <p className="project-desc px-3">{props.projectDescription}</p>
         {props.githubLink !== "" ? (
