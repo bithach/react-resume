a4c2d48c26100d654feeffe4f87f19c3ea14b23d
Thach Huynh <thh190001@utdallas.edu>
2023-11-22 12:00:35 -0600

    set tags at bottom of card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,11 +4,12 @@ export function ProjectCard(props) {
   return (
     <div className="project-card p-[5px]">
       <div
-        className="project-card-container text-center w-64 max-h-[450px] max-w-64 border-2 border-black rounded-2xl bg-white 
+        className="project-card-container text-center w-64 h-[450px] m-h-[450px] max-w-64 border-2 border-black rounded-2xl bg-white 
                       transition ease-out delay-400 
                       hover:translate-y-[-10px] 
                       hover:shadow-md 
-                      hover:shadow-black"
+                      hover:shadow-black
+                      relative"
       >
         <h3 className="project-card-name text-center font-bold py-[5px] border-b-2 border-slate-400">
           {props.projectName}
@@ -35,7 +36,7 @@ export function ProjectCard(props) {
           >
             <img
               src={githubIcon}
-              className="github-icon w-[25px] pb-[10px]"
+              className="github-icon w-[25px] pb-[10px] max-w-[25px]"
               alt="Github Logo"
             ></img>
           </a>
@@ -43,7 +44,7 @@ export function ProjectCard(props) {
           ""
         )}
 
-        <div className="project-tags border-slate-400 border-t-2 text-gray-700">
+        <div className="project-tags border-slate-400 border-t-2 text-gray-700 w-full absolute bottom-0">
           {props.projectTags}
         </div>
       </div>
