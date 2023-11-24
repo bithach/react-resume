7a2c4fd775426d89d001b59cc90906ee348edc49
Thach Huynh <thh190001@utdallas.edu>
2023-11-23 23:54:15 -0600

    consistent headers

--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -5,7 +5,7 @@ import projectsData from "../data/projectsData.json";
 function ProjectsSection(props) {
   return (
     <div className="projectsSection-body" id="projects">
-      <h1 className="projectsSection-title-desktop font-black text-center text-[#008000] underline pt-[20px] pb-[10px] md:text-[50px]">
+      <h1 className="projectsSection-title-desktop font-black text-center text-[#008000] underline py-[20px] md:text-[50px]">
         /* Projects *\
       </h1>
       <div className="projectsSection-card-container w-full justify-center flex flex-wrap">
--- src/sections/WorkExperienceSection.jsx
+++ src/sections/WorkExperienceSection.jsx
@@ -6,7 +6,7 @@ function WorkExperienceSection(props) {
       className="workExeperience-body pb-[100px] max-h-[540px]"
       id="experience"
     >
-      <h1 className="workExeperience-title-desktop font-black text-center text-[#008000] underline pt-[20px] pb-[20px] md:text-[50px]">
+      <h1 className="workExeperience-title-desktop font-black text-center text-[#008000] underline py-[20px] md:text-[50px]">
         /* Work Experience *\
       </h1>
       <WorkExperience />
