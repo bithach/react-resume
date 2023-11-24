c1096245cc3e3aa07e10ad6543fcbf86392d2088
Thach Huynh <thh190001@utdallas.edu>
2023-11-23 22:25:52 -0600

    add id

--- src/data/WorkExperienceData.jsx
+++ src/data/WorkExperienceData.jsx
@@ -4,5 +4,6 @@ export let experienceData = [
     description:
       "During my internship here, I was able to get hands on experience with React and primarily working in the Risk-Compliance department. With this experience, I was able to learn more about web development along with using different libraries concerning visualization of data and became more interested in the world of web development. ",
     logo: require("../../src/styling/images/Usaa_logo_PNG2.png"),
+    id: "1",
   },
 ];
