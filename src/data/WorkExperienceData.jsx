44346afb604b25301052ee621c162b40c4424039
Thach Huynh <thh190001@utdallas.edu>
2023-11-24 15:27:02 -0600

    remove max-h (messed up div spacing)

--- src/data/WorkExperienceData.jsx
+++ src/data/WorkExperienceData.jsx
@@ -3,7 +3,7 @@ export let experienceData = [
     companyName: "USAA",
     duration: "Summer 2023",
     description: [
-      "Developed apps in the React framework to display information from a XML file which is sent to another department to handle said information",
+      "Developed apps in the React framework to display information from a XML file that is sent to another department to handle the information being sent through the app",
       "Attended daily standups to check progress among the team members within the same department along with attending a spring planning to determine plans for the upcoming quarter",
       "Used a map library to gain a view of the demographics of the information to potentially focus on where to increase clients",
     ],
--- src/sections/WorkExperienceSection.jsx
+++ src/sections/WorkExperienceSection.jsx
@@ -2,10 +2,7 @@ import { WorkExperience } from "../components/WorkExperience";
 
 function WorkExperienceSection(props) {
   return (
-    <div
-      className="workExeperience-body pb-[100px] max-h-[540px]"
-      id="experience"
-    >
+    <div className="workExeperience-body pb-[100px]" id="experience">
       <h1 className="workExeperience-title-desktop font-black text-center text-[#008000] underline py-[20px] md:text-[50px]">
         /* Work Experience *\
       </h1>
